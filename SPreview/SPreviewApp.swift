4c273353320dc461dd9fb05b89afe72a176a3466
Amr Alharazi <haraziamr@gmail.com>
2023-09-30 09:45:42 +0300

    Initial Commit

---
+++ SPreview/SPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPreviewApp.swift
+//  SPreview
+//
+//  Created by Amr on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
